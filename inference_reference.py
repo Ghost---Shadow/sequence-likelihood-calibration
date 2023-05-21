8b9bd06dcf9b74bceb12f276ea38eac16afb09bc
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-05-21 08:15:17 +0200

    lint

--- inference_reference.py
+++ inference_reference.py
@@ -2,8 +2,8 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration, GenerationConf
 from datasets import load_dataset
 
 dataset = load_dataset("CarperAI/openai_summarize_comparisons")
-row = dataset['train'][0]
-prompt = row['prompt']
+row = dataset["train"][0]
+prompt = row["prompt"]
 
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
@@ -14,7 +14,7 @@ generation_config = GenerationConfig(max_length=1024)
 outputs = model.generate(input_ids, generation_config)
 
 print(prompt)
-print('---')
+print("---")
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-print('---')
+print("---")
 print(len(outputs[0]))

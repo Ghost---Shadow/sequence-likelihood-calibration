4c25db5be1744ca444f575195a256f6d1644dfd4
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-05-21 11:19:59 +0200

    train classifer

--- comparison_dataset.py
+++ comparison_dataset.py
@@ -1,3 +1,4 @@
+import random
 from torch.utils.data import Dataset, DataLoader
 from transformers import T5Tokenizer
 from datasets import load_dataset
@@ -8,32 +9,76 @@ class ComparisionDataset(Dataset):
     INSTRUCTION = "summarize: "
 
     def __init__(self, split, debug=False):
+        ComparisionDataset.tokenizer.add_tokens(["\n"])
         self.dataset = load_dataset("CarperAI/openai_summarize_comparisons")[split]
+
+        # self.dataset = self.dataset.filter(ComparisionDataset.filter_function)
+
         if debug:
-            self.dataset = self.dataset.select(range(5))
+            self.dataset = self.dataset.select(range(100))
 
     def __len__(self):
         return len(self.dataset)
 
+    @staticmethod
+    def filter_function(row):
+        row = ComparisionDataset.format_row(row)
+        all_text = row["full_prompt"] + row["correct_answer"]
+        tokens = ComparisionDataset.tokenizer(
+            all_text,
+            return_tensors="pt",
+        ).input_ids
+        return len(tokens) <= 512
+
+    @staticmethod
+    def format_row(row):
+        labels = ["A", "B"]
+        row["chosen"] = row["chosen"].replace("TL;DR: ", "").strip()
+        row["rejected"] = row["rejected"].replace("TL;DR: ", "").strip()
+        answers = [None, None]
+        prompt = row["prompt"]
+
+        prompt = prompt.split("POST: ")[1]
+        prompt = prompt.replace("\\r\\n", " ")
+        prompt = prompt.replace("\\n", " ")
+        prompt = prompt.replace("\r\n", " ")
+        prompt = prompt.replace("\n", " ")
+
+        correct_index = random.randint(0, 1)
+
+        correct_answer = labels[correct_index]
+        answers[correct_index] = row["chosen"]
+        answers[1 - correct_index] = row["rejected"]
+
+        # Create the full prompt
+        full_prompt = f"{prompt}\nA: {answers[0]}\nB: {answers[1]}"
+        full_prompt = full_prompt.strip()
+        full_prompt = f"{full_prompt}\n"
+
+        return {
+            "full_prompt": full_prompt,
+            "correct_answer": correct_answer,
+        }
+
     def __getitem__(self, idx):
-        return self.dataset[idx]
+        row = self.dataset[idx]
+        return ComparisionDataset.format_row(row)
 
     def sanity_check(self):
-        prompts = [ComparisionDataset.INSTRUCTION + self.dataset[0]["prompt"]]
+        prompts = [self[0]["full_prompt"]]
         input_ids = ComparisionDataset._tokenize(prompts)
 
         return (
             input_ids,
-            prompts[0],
-            self.dataset[0]["chosen"],
-            self.dataset[0]["rejected"],
+            self[0]["full_prompt"],
+            self[0]["correct_answer"],
         )
 
     @staticmethod
-    def _tokenize(s):
+    def _tokenize(s, padding=True):
         tokens = ComparisionDataset.tokenizer(
             s,
-            padding=True,
+            padding=padding,
             truncation=True,
             return_tensors="pt",
         ).input_ids
@@ -41,27 +86,44 @@ class ComparisionDataset(Dataset):
 
     @staticmethod
     def collate_fn(batch):
-        prompts = [ComparisionDataset.INSTRUCTION + item["prompt"] for item in batch]
-        chosen_labels = [item["chosen"] for item in batch]
-        rejected_labels = [item["rejected"] for item in batch]
+        input_ids = [item["full_prompt"] for item in batch]
+        labels = [item["correct_answer"] for item in batch]
 
-        input_ids = ComparisionDataset._tokenize(prompts)
-        chosen_labels = ComparisionDataset._tokenize(chosen_labels)
-        rejected_labels = ComparisionDataset._tokenize(rejected_labels)
+        input_ids = ComparisionDataset._tokenize(input_ids)
+        labels = ComparisionDataset._tokenize(labels, padding=False)
 
         return {
             "input_ids": input_ids,
-            "chosen_labels": chosen_labels,
-            "rejected_labels": rejected_labels,
+            "labels": labels,
         }
 
 
+def test_dataloader(dataloader):
+    # Open a txt file
+    with open("dataloader_output.txt", "w") as f:
+        # Loop over the first 5 items of dataloader
+        for i, row in enumerate(dataloader):
+            if i >= 5:  # only need the first 5 items
+                break
+
+            # Detokenize input_ids and labels
+            input_texts = ComparisionDataset.tokenizer.batch_decode(
+                row["input_ids"], skip_special_tokens=True
+            )
+            label_texts = ComparisionDataset.tokenizer.batch_decode(
+                row["labels"], skip_special_tokens=True
+            )
+
+            # Write detokenized texts to txt file
+            for input_text, label_text in zip(input_texts, label_texts):
+                f.write(f"{input_text}{label_text}\n")
+                f.write("---\n")
+
+
 if __name__ == "__main__":
-    dataset = ComparisionDataset(split="train")
+    dataset = ComparisionDataset(split="train", debug=True)
     dataloader = DataLoader(
         dataset, batch_size=2, shuffle=True, collate_fn=ComparisionDataset.collate_fn
     )
 
-    for row in dataloader:
-        print(row)
-        break
+    test_dataloader(dataloader)
---
+++ train_classifier.py
@@ -0,0 +1,91 @@
+from tqdm import tqdm
+from transformers import T5ForConditionalGeneration
+from comparison_dataset import ComparisionDataset
+from torch.utils.data import DataLoader
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+model.train()
+
+EPOCHS = 3
+BATCH_SIZE = 2
+LR = 1e-3
+DEBUG = False
+
+train_dataset = ComparisionDataset("train", debug=DEBUG)
+val_dataset = ComparisionDataset("valid1", debug=DEBUG)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    collate_fn=ComparisionDataset.collate_fn,
+)
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    collate_fn=ComparisionDataset.collate_fn,
+)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
+
+device = "cuda"
+
+# Initialize TensorBoard writer
+writer = SummaryWriter()
+global_steps = 0
+
+for epoch in range(EPOCHS):
+    model.train().to(device)
+    for batch in tqdm(train_loader):
+        optimizer.zero_grad()
+        input_ids = batch["input_ids"].to(device)
+        labels = batch["labels"].to(device)
+
+        outputs = model(input_ids=input_ids, labels=labels)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+
+        # Log training loss to TensorBoard
+        writer.add_scalar("Loss/train", loss.item(), global_steps)
+        global_steps += 1
+
+    total_loss = 0
+    model.eval().to(device)
+    with torch.no_grad():
+        for batch in tqdm(val_loader):
+            input_ids = batch["input_ids"].to(device)
+            labels = batch["labels"].to(device)
+
+            outputs = model(input_ids=input_ids, labels=labels)
+            loss = outputs.loss
+
+            total_loss += loss.item()
+
+    val_loss = total_loss / len(val_loader)
+    print(f"Validation loss {val_loss}")
+
+    # Log validation loss to TensorBoard
+    writer.add_scalar("Loss/validation", val_loss, epoch)
+
+    # Generate a sample text and log it to TensorBoard
+    with torch.no_grad():
+        sample_input_ids, prompt, expected = val_dataset.sanity_check()
+        # https://stackoverflow.com/a/52784607/1217998
+        prompt = prompt.replace("\n", "  \n")
+        sample_input_ids = sample_input_ids.to(device)
+        model.eval()
+        generated = model.generate(sample_input_ids, max_length=100, temperature=0.0)
+        model.train()
+        generated_text = ComparisionDataset.tokenizer.decode(generated[0])
+        writer.add_text(
+            "Text Generation",
+            f"### Input ###\n\n{prompt}\n\n### Expected ###\n\n{expected}\n\n### Output ###\n\n{generated_text}",
+            epoch,
+        )
+
+# Close the writer
+writer.close()

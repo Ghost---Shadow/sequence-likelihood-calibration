c8a0f7708f6a6ca1844b81a587b18ef2af7978a9
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-05-21 08:34:55 +0200

    Tensorboard logs

--- sft_dataset.py
+++ sft_dataset.py
@@ -2,13 +2,16 @@ from torch.utils.data import Dataset, DataLoader
 from transformers import T5Tokenizer
 from datasets import load_dataset
 
+
 class SftDataset(Dataset):
     tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    INSTRUCTION = "summarize: "
+
     def __init__(self, split, debug=False):
         # dataset = load_dataset("CarperAI/openai_summarize_comparisons")
         self.dataset = load_dataset("CarperAI/openai_summarize_tldr")[split]
         if debug:
-            self.dataset = self.dataset.select(range(100))
+            self.dataset = self.dataset.select(range(5))
 
     def __len__(self):
         return len(self.dataset)
@@ -16,24 +19,41 @@ class SftDataset(Dataset):
     def __getitem__(self, idx):
         return self.dataset[idx]
 
+    def sanity_check(self):
+        prompts = [SftDataset.INSTRUCTION + self.dataset[0]["prompt"]]
+        input_ids = SftDataset._tokenize(prompts)
+
+        return input_ids, prompts[0], self.dataset[0]["label"]
+
+    @staticmethod
+    def _tokenize(s):
+        tokens = SftDataset.tokenizer(
+            s,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+        ).input_ids
+        return tokens
+
     @staticmethod
     def collate_fn(batch):
-        INSTRUCTION = 'summarize: '
+        prompts = [SftDataset.INSTRUCTION + item["prompt"] for item in batch]
+        labels = [item["label"] for item in batch]
 
-        prompts = [INSTRUCTION + item['prompt'] for item in batch]
-        labels = [item['label'] for item in batch]
-
-        input_ids = SftDataset.tokenizer(prompts, padding=True, truncation=True, return_tensors="pt").input_ids
-        labels = SftDataset.tokenizer(labels, padding=True, truncation=True, return_tensors="pt").input_ids
+        input_ids = SftDataset._tokenize(prompts)
+        labels = SftDataset._tokenize(labels)
 
         return {
-            'input_ids': input_ids,
-            'labels': labels,
+            "input_ids": input_ids,
+            "labels": labels,
         }
 
+
 if __name__ == "__main__":
-    dataset = SftDataset(split='train')
-    dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=SftDataset.collate_fn)
+    dataset = SftDataset(split="train")
+    dataloader = DataLoader(
+        dataset, batch_size=2, shuffle=True, collate_fn=SftDataset.collate_fn
+    )
 
     for row in dataloader:
         print(row)
--- train_sft.py
+++ train_sft.py
@@ -3,32 +3,40 @@ from transformers import T5ForConditionalGeneration
 from sft_dataset import SftDataset
 from torch.utils.data import DataLoader
 import torch
+from torch.utils.tensorboard import SummaryWriter
 
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
 model.train()
 
-train_dataset = SftDataset('train')
-val_dataset = SftDataset('valid')
-
 EPOCHS = 3
 BATCH_SIZE = 2
 LR = 1e-3
+DEBUG = True
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=SftDataset.collate_fn)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=SftDataset.collate_fn)
+train_dataset = SftDataset("train", debug=DEBUG)
+val_dataset = SftDataset("valid", debug=DEBUG)
+
+train_loader = DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=SftDataset.collate_fn
+)
+val_loader = DataLoader(
+    val_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=SftDataset.collate_fn
+)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
-criterion = torch.nn.CrossEntropyLoss()
 
-device = 'cuda'
+device = "cuda"
+
+# Initialize TensorBoard writer
+writer = SummaryWriter()
 
 for epoch in range(EPOCHS):
     total_loss = 0
     model.train().to(device)
     for batch in tqdm(train_loader):
         optimizer.zero_grad()
-        input_ids = batch['input_ids'].to(device)
-        labels = batch['labels'].to(device)
+        input_ids = batch["input_ids"].to(device)
+        labels = batch["labels"].to(device)
 
         outputs = model(input_ids=input_ids, labels=labels)
         loss = outputs.loss
@@ -37,18 +45,41 @@ for epoch in range(EPOCHS):
 
         total_loss += loss.item()
 
-    print(f'Train loss {total_loss / len(train_loader)}')
+    train_loss = total_loss / len(train_loader)
+    print(f"Train loss {train_loss}")
+
+    # Log training loss to TensorBoard
+    writer.add_scalar("Loss/train", train_loss, epoch)
 
     total_loss = 0
     model.eval().to(device)
     with torch.no_grad():
         for batch in tqdm(val_loader):
-            input_ids = batch['input_ids'].to(device)
-            labels = batch['labels'].to(device)
+            input_ids = batch["input_ids"].to(device)
+            labels = batch["labels"].to(device)
 
             outputs = model(input_ids=input_ids, labels=labels)
             loss = outputs.loss
 
             total_loss += loss.item()
 
-    print(f'Validation loss {total_loss / len(val_loader)}')
+    val_loss = total_loss / len(val_loader)
+    print(f"Validation loss {val_loss}")
+
+    # Log validation loss to TensorBoard
+    writer.add_scalar("Loss/validation", val_loss, epoch)
+
+    # Generate a sample text and log it to TensorBoard
+    with torch.no_grad():
+        sample_input_ids, prompt, expected = val_dataset.sanity_check()
+        sample_input_ids = sample_input_ids.to(device)
+        generated = model.generate(sample_input_ids, max_length=100, temperature=0.0)
+        generated_text = SftDataset.tokenizer.decode(generated[0])
+        writer.add_text(
+            "Text Generation",
+            f"### Input ###\n\n{prompt}\n\n### Expected ###\n\n{expected}\n\n### Output ###\n\n{generated_text}",
+            epoch,
+        )
+
+# Close the writer
+writer.close()

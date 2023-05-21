2de7bf33b36c2036015a53340e2f695a2133fd3b
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-05-21 17:49:10 +0200

    setup py boilerplate

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Load requirements
+with open("requirements.txt", "r") as req_file:
+    requirements = req_file.readlines()
+
+setup(
+    name="Your-Package-Name",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your-email@example.com",
+    description="A brief description of your project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/yourusername/your-repo",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)

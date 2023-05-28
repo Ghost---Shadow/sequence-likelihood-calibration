21ae7fabdf4d53dd11336bd5e132a1e2e4d182b7
Souradeep Nanda <souradeep.nanda123@gmail.com>
2023-05-28 17:53:57 +0000

    do everything update

--- src/do_everything_large.sh
+++ src/do_everything_large.sh
@@ -1,9 +1,9 @@
 set -o pipefail
 
 python src/offline/summary_generation.py --split=train --model-name=t5-base --batch-size=50 --limit=1000
-python src/offline/summary_generation.py --split=valid --model-name=t5-base --batch-size=50 --limit=1000
+python src/offline/summary_generation.py --split=valid --model-name=t5-base --batch-size=50 --limit=100
 
 python src/offline/summary_classification_length.py
 
-python src/train/train_slic.py --loss-type=slic_loss --model-name=t5-base --limit=1000
-python src/train/train_slic.py --loss-type=slic_loss_logits --model-name=t5-base --limit=1000
+python src/train/train_slic.py --model-name=t5-base --batch-size=6 --limit=1000 --loss-type=slic_loss 
+python src/train/train_slic.py --model-name=t5-base --batch-size=6 --limit=1000 --loss-type=slic_loss_logits 
